5a22c3126a01d3d0dee434975563c52a3cfe1165
Vidityar <van.adith@gmail.com>
2023-06-04 02:38:20 +0700

    add: home pages to the project

---
+++ src/pages/galleryPages.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import ImageGallery from "../components/ImageGallery";
+
+function GalleryPages() {
+  return (
+    <>
+      <Navbar />
+      <ImageGallery />
+    </>
+  );
+}
+
+export default GalleryPages;
